f85bce33fe933adc67736004f707f083c619ffdd
Zachary Whaley <zachbwhaley@gmail.com>
2016-10-03 16:00:52 -0500

    p4-completion v1.0 (new formula)

---
+++ p4-completion.rb
@@ -0,0 +1,11 @@
+class P4Completion < Formula
+  desc "P4 Bash Completion"
+  head "https://github.com/zachwhaley/p4_completion.git"
+  url "https://github.com/zachwhaley/p4_completion/archive/v1.0.tar.gz"
+  sha256 "e64d54375bd021b19392b6065ceba31507f93686a8df0589e4071f424cad8579"
+  depends_on "bash-completion"
+
+  def install
+    FileUtils.install "p4_completion.bash", "#{etc}/bash_completion/p4", :mode => 0444
+  end
+end
