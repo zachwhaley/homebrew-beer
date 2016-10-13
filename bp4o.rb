a2f084f699f3ceb809ba7d7dd69d4fab1663ed10
Zach Whaley <zachbwhaley@gmail.com>
2016-10-13 07:31:48 -0500

    Update bp4o to v0.3.1

--- bp4o.rb
+++ bp4o.rb
@@ -2,12 +2,12 @@ class Bp4o < Formula
   desc "Better P4 Output"
   homepage "http://zachwhaleys.website/bp4o/"
   head "https://github.com/zachwhaley/bp4o.git"
-  url "https://github.com/zachwhaley/bp4o/archive/v0.3.0.tar.gz"
-  sha256 "33fea23ee1efb6ac5ca7d36aeb9ad41a2062014ac2ece8d4f61daba4e0235913"
+  url "https://github.com/zachwhaley/bp4o/archive/v0.3.1.tar.gz"
+  sha256 "a4cb998068e1d980231ee0f5386fe72eb42b823c4f4bed8209ecbca1de182d61"
 
   def install
     bin.install Dir["bin/*"]
-    pkgshare.install Dir["./init.*"]
+    pkgshare.install Dir["./bp4o.*"]
   end
 
   def caveats
@@ -15,7 +15,7 @@ class Bp4o < Formula
     init = <<-EOS.undent
       Add the following to your .#{shell}rc file to setup BP4O:
 
-      [ -f /usr/local/share/bp4o/init.#{shell} ] && source /usr/local/share/bp4o/init.#{shell}
+        [ -f /usr/local/share/bp4o/bp4o.#{shell} ] && source /usr/local/share/bp4o/bp4o.#{shell}
     EOS
     init
   end
