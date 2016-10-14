ecd857ea74647888de51ca1fae408a84c9dab2d0
Zach Whaley <zachbwhaley@gmail.com>
2016-10-14 14:18:25 -0500

    Update bp4o to v0.3.2

--- bp4o.rb
+++ bp4o.rb
@@ -2,8 +2,8 @@ class Bp4o < Formula
   desc "Better P4 Output"
   homepage "http://zachwhaleys.website/bp4o/"
   head "https://github.com/zachwhaley/bp4o.git"
-  url "https://github.com/zachwhaley/bp4o/archive/v0.3.1.tar.gz"
-  sha256 "a4cb998068e1d980231ee0f5386fe72eb42b823c4f4bed8209ecbca1de182d61"
+  url "https://github.com/zachwhaley/bp4o/archive/v0.3.2.tar.gz"
+  sha256 "80c370c8359273c6f789858a804a6179ecd87ae1de8b5108363f8d66ad2677f0"
 
   def install
     bin.install Dir["bin/*"]
