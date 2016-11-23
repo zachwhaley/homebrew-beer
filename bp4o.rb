a9ed334905127be8f30bb8c159c412c3c861e9b0
Zach Whaley <zachbwhaley@gmail.com>
2016-11-22 20:34:16 -0600

    Update bp4o to v0.4.0

--- bp4o.rb
+++ bp4o.rb
@@ -2,13 +2,14 @@ class Bp4o < Formula
   desc "Better P4 Output"
   homepage "http://zachwhaleys.website/bp4o/"
   head "https://github.com/zachwhaley/bp4o.git"
-  url "https://github.com/zachwhaley/bp4o/archive/v0.3.3.tar.gz"
-  sha256 "c191e1376d1d4f9773e8ff994312d801ea917106a1e5f1111d1e60504d12d81d"
+  url "https://github.com/zachwhaley/bp4o/archive/v0.4.0.tar.gz"
+  sha256 "224dc3a95bf966d4a4cd7b9b34c966cfa5347143c50235f4adde2537521eacf3"
 
   def install
     bin.install Dir["bin/*"]
     prefix+"etc/profile.d".install "bp4o.bash" => "bp4o.sh"
     share+"zsh/site-functions".install "bp4o.zsh" => "bp4o"
+    share+"fish/vendor_functions.d".install "bp4o.fish" => "p4.fish"
   end
 
   def caveats
